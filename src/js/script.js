906cd26e9f97dda1e5760b6b9b824f6e07f2f56c
Przemek Bochman <przemyslaw.bochman@gmail.com>
2021-08-10 23:11:05 +0200

    working prices

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,10 @@
       thisProduct.data = data;
     
       thisProduct.renderInMenu();
-      thisProduct,this.initAccordion();
+      thisProduct.getElements();
+      thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
     }
 
     renderInMenu()  {
@@ -71,15 +74,22 @@
       manuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion() {
       const thisProduct = this;
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         event.preventDefault();
         let selectedItem = thisProduct.id;
-        console.log('selectedItem',selectedItem);
         const articles = document.getElementsByClassName('product');    
-        
         for (let article of articles)
         {
           if (selectedItem == article.id)
@@ -90,13 +100,62 @@
 
       });   
     }
+
+    initOrderForm() {
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder()  {
+      const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      let price = thisProduct.data.price;
+
+
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            if(!option.default == true)  {
+              price += option.price;
+            }
+          }
+          else {
+            if(option.default == true)  {
+              price -= option.price;
+            }
+          }
+        }
+      }
+      console.log('Price',price);
+      thisProduct.priceElem.innerHTML = price;
+    }
+
   }
 
 
   const app = {
     initMenu: function() {
       const thisApp = this;
-      //console.log('thisApp.data: ', thisApp.data);
       for (let productData in thisApp.data.products)  {
         new Product(productData, thisApp.data.products[productData]);
       }
@@ -109,12 +168,6 @@
 
     init: function(){
       const thisApp = this;
-      //console.log('*** App starting ***');
-      //console.log('thisApp:', thisApp);
-      //console.log('classNames:', classNames);
-      //console.log('settings:', settings);
-      //console.log('templates:', templates);
-
       thisApp.initData();
       thisApp.initMenu();
     },
